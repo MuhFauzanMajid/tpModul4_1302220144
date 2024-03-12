54fe3d65e8d699ad96b8fdaf249b555c5798f5fb
MuhFauzanMajid <muhammadfauzanmajid@gmail.com>
2024-03-12 14:00:47 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul4_1302220144/KodePos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302220144
+{
+    class KodePos
+    {
+        private static Dictionary<string, int> tabelKodePos = new Dictionary<string, int>
+    {
+        {"Batununggal", 40266},
+        {"Kujangsar", 40287},
+        {"Mengger", 40267},
+        {"Wales", 40256},
+        {"Cijaura", 40287},
+        {"Jatisari", 40286},
+        {"Margasari", 40286},
+        {"Sekejati", 40286},
+        {"Kebonwaru", 40272},
+        {"Maleer", 40274},
+        {"Samoja", 40273}
+    };
+
+        public int getKodePos(string lokasi)
+        {
+            if (tabelKodePos.ContainsKey(lokasi))
+            {
+                return tabelKodePos[lokasi];
+            }
+            else
+            {
+                return 0; // atau bisa mengembalikan nilai lain jika kode pos tidak ditemukan
+            }
+        }
+    }
+}
--- tpmodul4_1302220144/Program.cs
+++ tpmodul4_1302220144/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using tpmodul4_1302220144;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        KodePos kodePos = new KodePos();
+
+        Console.WriteLine("Masukkan nama lokasi (ketik 'exit' untuk keluar):");
+
+        string input = Console.ReadLine();
+
+        while (input.ToLower() != "exit")
+        {
+            int kodePosTempat = kodePos.getKodePos(input);
+
+            if (kodePosTempat == 0)
+            {
+                Console.WriteLine("Kode pos untuk lokasi " + input + " tidak ditemukan.");
+            }
+            else
+            {
+                Console.WriteLine("Kode pos untuk " + input + ": " + kodePosTempat);
+            }
+
+            Console.WriteLine("Masukkan nama lokasi (ketik 'exit' untuk keluar):");
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine("Terima kasih telah menggunakan program ini.");
+        Console.ReadLine();
+    }
+}
