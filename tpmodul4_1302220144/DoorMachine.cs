9929d74c8ca09077cfabdbeb57f5526dc06d5c72
MuhFauzanMajid <muhammadfauzanmajid@gmail.com>
2024-03-12 14:33:05 +0700

    menambahkan implementasi dengan state-based construction

---
+++ tpmodul4_1302220144/DoorMachine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302220144
+{
+    enum DoorState
+    {
+        Terkunci,
+        Terbuka
+    }
+    class DoorMachine
+    {
+        private DoorState currentState;
+
+        public DoorMachine()
+        {
+            // State awal adalah Terkunci
+            currentState = DoorState.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void BukaPintu()
+        {
+            if (currentState == DoorState.Terkunci)
+            {
+                currentState = DoorState.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah tidak terkunci");
+            }
+        }
+
+        public void KunciPintu()
+        {
+            if (currentState == DoorState.Terbuka)
+            {
+                currentState = DoorState.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terkunci");
+            }
+        }
+    }
+
+}
--- tpmodul4_1302220144/Program.cs
+++ tpmodul4_1302220144/Program.cs
@@ -4,30 +4,38 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Testing KodePos");
+        Console.WriteLine();
         KodePos kodePos = new KodePos();
 
-        Console.WriteLine("Masukkan nama lokasi (ketik 'exit' untuk keluar):");
+        Console.Write("Masukkan nama lokasi : ");
 
         string input = Console.ReadLine();
+       
+        int kodePosTempat = kodePos.getKodePos(input);
 
-        while (input.ToLower() != "exit")
+        if (kodePosTempat == 0)
         {
-            int kodePosTempat = kodePos.getKodePos(input);
-
-            if (kodePosTempat == 0)
-            {
-                Console.WriteLine("Kode pos untuk lokasi " + input + " tidak ditemukan.");
-            }
-            else
-            {
-                Console.WriteLine("Kode pos untuk " + input + ": " + kodePosTempat);
-            }
-
-            Console.WriteLine("Masukkan nama lokasi (ketik 'exit' untuk keluar):");
-            input = Console.ReadLine();
+            Console.WriteLine("Kode pos untuk lokasi " + input + " tidak ditemukan.");
+        }
+        else
+        {
+            Console.WriteLine("Kode pos untuk " + input + ": " + kodePosTempat);
         }
 
-        Console.WriteLine("Terima kasih telah menggunakan program ini.");
-        Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("Testing DoorState");
+        Console.WriteLine();
+
+        DoorMachine pintu = new DoorMachine();
+        // Simulasi perubahan state
+        Console.WriteLine("\nMencoba membuka pintu...");
+        pintu.BukaPintu();
+
+        Console.WriteLine("\nMencoba mengunci pintu...");
+        pintu.KunciPintu();
+
+        Console.WriteLine("\nMencoba membuka pintu lagi...");
+        pintu.BukaPintu();
     }
 }
